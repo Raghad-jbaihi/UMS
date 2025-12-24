099b69074d531d853c7e2aa220ef3e8d85d959e8
raghad-jbaihi <jbihiraghad@gmail.com>
2025-12-24 17:25:35 +0200

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,21 @@
+const getUsers= async()=>{
+    const response =await axios.get(`http://ums12.runasp.net/api/users`);
+    return response.data;
+}
+
+
+const dusplayUsers =async()=>{
+    const result =await getUsers();
+    const users=result.users.map((user)=>{
+        return  `
+        <tr>
+        <td>${user.name}</td>
+        <td><img src="${user.imageUrl}"/></td>
+        </tr> 
+        `
+
+    }
+).join('');
+document.querySelector(".users .users_data").innerHTML=users;
+}
+dispalyUsers();
