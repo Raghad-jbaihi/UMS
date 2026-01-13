f87b6ccdc8a15d1856ffcd6af16f48d826b6e975
raghad-jbaihi <jbihiraghad@gmail.com>
2026-01-13 18:01:21 +0200

    changes create.js code comment

--- assets/js/create.js
+++ assets/js/create.js
@@ -1,20 +1,7 @@
-const createUserForm = document.forms['AddUserForm'];
-
-createUserForm.addEventListener("submit", async (e) => {
-    e.preventDefault();
-    const formData = new FormData(createUserForm);
-
-    try {
-        const response = await axios.post(
-            `https://ums12.runasp.net/api/users`,
-            formData
-        );
-
-        console.log(response);
-        alert("User created successfully!");
-        
-    } catch (error) {
-        console.error(error);
-        alert("Failed to create user.");
-    }
-});
+const createUserForm =document.forms['AddUserForm'];
+createUserForm .addEventListener("submit",async(e)=>{
+e.preventDefault();
+const formData = new FormData(createUserForm);
+const response =await axios.post(`https://ums12.runasp.net/api/users`,formData);
+console.log(response);
+})
